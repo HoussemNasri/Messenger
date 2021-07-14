124ef4c781a3fd4890bced0c957500b66abd8ada
Houssem Nasri <housi.housi2015@gmail.com>
2021-07-14 19:31:30 +0100

    Document MessengerDateFormatter

--- app/src/main/java/com/nasri/messenger/ui/chats/MessengerDateFormatter.kt
+++ app/src/main/java/com/nasri/messenger/ui/chats/MessengerDateFormatter.kt
@@ -1,17 +1,18 @@
 package com.nasri.messenger.ui.chats
 
-import timber.log.Timber
-import java.lang.IllegalArgumentException
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
+/**
+ * A utility class to handle date formatting.
+ * */
 class MessengerDateFormatter {
     companion object {
         /**
-         *
+         * Returns a user-friendly formatted date used primarily in message delivery
          * @throws IllegalArgumentException when the given date is after the current date
          * */
         fun formatMessageDeliveryDate(timestamp: Long): String {
@@ -21,18 +22,21 @@ class MessengerDateFormatter {
             if (now.isBefore(date)) {
                 throw IllegalArgumentException("Got you! time traveler")
             }
+            // Message delivered on the same year
             if (now.year == date.year) {
                 return when {
+                    // Message delivered on the same day
                     now.dayOfYear == date.dayOfYear -> date.format(DateTimeFormatter.ofPattern("HH:mm"))
-
+                    //Message delivered yesterday
                     ChronoUnit.DAYS.between(date, now) == 1L -> "Yesterday"
-
+                    //Message delivered this week
                     ChronoUnit.DAYS.between(date, now) <= 7 ->
                         date.format(DateTimeFormatter.ofPattern("E"))
-
+                    //Message delivered any time this year
                     else -> date.format(DateTimeFormatter.ofPattern("MMM dd"))
                 }
             }
+            // Message delivered in previous years
             return date.format(DateTimeFormatter.ofPattern("MMM dd, yyyy"))
         }
     }
